31add16346519d07a48488fdb83aade818ad39de
mortalkomabat5 <sriramkothuru513@gmail.com>
2025-11-07 15:31:36 +0530

    Update 05_iam.tf

--- terraform/05_iam.tf
+++ terraform/05_iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ecs-host-role" {
   name               = "ecs_host_role_prod"
-  assume_role_policy = file("ecs-role.json")
+  assume_role_policy = file("${path.module}/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-instance-role-policy" {
   name   = "ecs_instance_role_policy"
-  policy = file("ecs-instance-role-policy.json")
+  policy = file("${path.module}/policies/ecs-instance-role-policy.json")
   role   = aws_iam_role.ecs-host-role.id
 }
 
 resource "aws_iam_role" "ecs-service-role" {
   name               = "ecs_service_role_prod"
-  assume_role_policy = file("ecs-role.json")
+  assume_role_policy = file("${path.module}/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-service-role-policy" {
   name   = "ecs_service_role_policy"
-  policy = file("ecs-service-role-policy.json")
+  policy = file("${path.module}/policies/ecs-service-role-policy.json")
   role   = aws_iam_role.ecs-service-role.id
 }
 
