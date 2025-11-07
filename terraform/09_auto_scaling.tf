c0066054a4aa5992351366d027ffbade28cc1887
mortalkomabat5 <sriramkothuru513@gmail.com>
2025-11-07 15:32:21 +0530

    Update 09_auto_scaling.tf

--- terraform/09_auto_scaling.tf
+++ terraform/09_auto_scaling.tf
@@ -1,9 +1,19 @@
 resource "aws_autoscaling_group" "ecs-cluster" {
-  name                 = "${var.ecs_cluster_name}_auto_scaling_group"
-  min_size             = "${var.autoscale_min}"
-  max_size             = "${var.autoscale_max}"
-  desired_capacity     = "${var.autoscale_desired}"
-  health_check_type    = "EC2"
-  launch_configuration = aws_launch_configuration.ecs.name
-  vpc_zone_identifier  = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  name                = "${var.ecs_cluster_name}_auto_scaling_group"
+  min_size            = var.autoscale_min
+  max_size            = var.autoscale_max
+  desired_capacity    = var.autoscale_desired
+  health_check_type   = "EC2"
+  vpc_zone_identifier = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+
+  launch_template {
+    id      = aws_launch_template.ecs.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.ecs_cluster_name}-ecs-instance"
+    propagate_at_launch = true
+  }
 }
