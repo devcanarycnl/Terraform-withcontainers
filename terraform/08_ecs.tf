b77f6462db35a0ad3ca90e6419fe0b4a15b748a2
mortalkomabat5 <sriramkothuru513@gmail.com>
2025-11-07 15:32:18 +0530

    Update 08_ecs.tf

--- terraform/08_ecs.tf
+++ terraform/08_ecs.tf
@@ -2,15 +2,31 @@ resource "aws_ecs_cluster" "production" {
   name = "${var.ecs_cluster_name}-cluster"
 }
 
-resource "aws_launch_configuration" "ecs" {
-  name                        = "${var.ecs_cluster_name}-cluster"
-  image_id                    = lookup(var.amis, var.region)
-  instance_type               = var.instance_type
-  security_groups             = [aws_security_group.ecs.id]
-  iam_instance_profile        = aws_iam_instance_profile.ecs.name
-  key_name                    = aws_key_pair.production.key_name
-  associate_public_ip_address = true
-  user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}-cluster' > /etc/ecs/ecs.config"
+resource "aws_launch_template" "ecs" {
+  name                = "${var.ecs_cluster_name}-cluster"
+  image_id            = lookup(var.amis, var.region)
+  instance_type       = var.instance_type
+  
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs.name
+  }
+
+  key_name = aws_key_pair.production.key_name
+  
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo ECS_CLUSTER='${var.ecs_cluster_name}-cluster' > /etc/ecs/ecs.config
+              EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 data "template_file" "app" {
@@ -33,11 +49,11 @@ resource "aws_ecs_service" "production" {
   task_definition = aws_ecs_task_definition.app.arn
   iam_role        = aws_iam_role.ecs-service-role.arn
   desired_count   = var.app_count
-  depends_on      = [aws_alb_listener.ecs-alb-http-listener, aws_iam_role_policy.ecs-service-role-policy]
+  depends_on      = [aws_elb.production, aws_iam_role_policy.ecs-service-role-policy]
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.default-target-group.arn
-    container_name   = "django-app"
-    container_port   = 8000
+    elb_name       = aws_elb.production.name
+    container_name = "django-app"
+    container_port = 8000
   }
 }
